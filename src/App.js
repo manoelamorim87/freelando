7bd612c742181bc0ef3ed53a37db0c6d9ba7ba28
Manoel Amorim <manoelamorim87@gmail.com>
2023-05-23 11:42:33 -0300

    adicionando componente ListaSuspensa

--- src/App.js
+++ src/App.js
@@ -17,10 +17,40 @@ import { ProvedorTema } from "./componentes/ProvedorTema/ProvedorTema";
 import { Rodape } from "./componentes/Rodape/Rodape";
 import { Tipografia } from "./componentes/Tipografia/Tipografia";
 
+const estadosBrasileiros = [
+  { "text": "Acre", "value": "AC" },
+  { "text": "Alagoas", "value": "AL" },
+  { "text": "Amapá", "value": "AP" },
+  { "text": "Amazonas", "value": "AM" },
+  { "text": "Bahia", "value": "BA" },
+  { "text": "Ceará", "value": "CE" },
+  { "text": "Distrito Federal", "value": "DF" },
+  { "text": "Espírito Santo", "value": "ES" },
+  { "text": "Goiás", "value": "GO" },
+  { "text": "Maranhão", "value": "MA" },
+  { "text": "Mato Grosso", "value": "MT" },
+  { "text": "Mato Grosso do Sul", "value": "MS" },
+  { "text": "Minas Gerais", "value": "MG" },
+  { "text": "Pará", "value": "PA" },
+  { "text": "Paraíba", "value": "PB" },
+  { "text": "Paraná", "value": "PR" },
+  { "text": "Pernambuco", "value": "PE" },
+  { "text": "Piauí", "value": "PI" },
+  { "text": "Rio de Janeiro", "value": "RJ" },
+  { "text": "Rio Grande do Norte", "value": "RN" },
+  { "text": "Rio Grande do Sul", "value": "RS" },
+  { "text": "Rondônia", "value": "RO" },
+  { "text": "Roraima", "value": "RR" },
+  { "text": "Santa Catarina", "value": "SC" },
+  { "text": "São Paulo", "value": "SP" },
+  { "text": "Sergipe", "value": "SE" },
+  { "text": "Tocantins", "value": "TO" }
+]
 
 function App() {
   return (
     <ProvedorTema>
+      <Estilos />
       <Cabecalho>
         <Container>
           <Row align="center">
@@ -33,62 +63,58 @@ function App() {
           </Row>
         </Container>
       </Cabecalho>
-      <Estilos />
-      <Container style={{ margin: '80px 0' }}>
-        <Row justify="center">
-          <Col lg={6} md={8} sm={12}>
-            <Card>
-              <Tipografia variante="h1" componente="h1">
-                Freelando
-              </Tipografia>
-              <Tipografia variante="body" componente="body">
-                Crie seu perfil gratuitamente para começar a trabalhar com os melhores freelancers.
-                Em seguida, você poderá dar mais detalhes sobre suas demandas e sobre sua forma de trabalho.
-              </Tipografia>
-              <Row>
-                <Col>
-                  <CampoTexto titulo="Nome completo" />
-                </Col>
-              </Row>
-              <Row>
-                <Col lg={4} md={4} sm={4}>
-                  <CampoTexto titulo="Estado" />
-                </Col>
-                <Col lg={8} md={8} sm={8}>
-                  <CampoTexto titulo="Cidade" />
-                </Col>
-              </Row>
-              <Row>
-                <Col>
-                  <CampoTexto titulo="Email" />
-                </Col>
-              </Row>
-              <Row>
-                <Col lg={6} md={6} sm={6}>
-                  <CampoTexto titulo="Senha" />
-                </Col>
-                <Col lg={6} md={6} sm={6}>
-                  <CampoTexto titulo="Repita a senha" />
-                </Col>
-              </Row>
-              <Row>
-                <Col lg={6} md={6} sm={6}>
-                  <Botao variante="secundaria">
-                    Anterior
+      <Row justify="center">
+        <Col xxx={6} xxl={6} xl={6} lg={6} md={8} sm={12} style={{ margin: '80px 0' }}>
+          <Card>
+            <Tipografia variante="h1" componente="h1">
+              Freelando
+            </Tipografia>
+            <Tipografia variante="body" componente="body">
+              Crie seu perfil gratuitamente para começar a trabalhar com os melhores freelancers. Em seguida, você poderá dar mais detalhes sobre suas demandas e sobre sua forma de trabalho.
+            </Tipografia>
+            <Row>
+              <Col>
+                <CampoTexto titulo="Nome Completo" />
+              </Col>
+            </Row>
+            <Row>
+              <Col lg={4} md={4} sm={4}>
+                <ListaSupensa titulo="Estado" opcoes={estadosBrasileiros} />
+              </Col>
+              <Col lg={8} md={8} sm={8}>
+                <CampoTexto titulo="Cidade" />
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <CampoTexto titulo="E-mail" />
+              </Col>
+            </Row>
+            <Row>
+              <Col lg={6} md={6} sm={6}>
+                <CampoTexto titulo="Senha" />
+              </Col>
+              <Col lg={6} md={6} sm={6}>
+                <CampoTexto titulo="Repita a Senha" />
+              </Col>
+            </Row>
+            <Row>
+              <Col lg={6} md={6} sm={6}>
+                <Botao variante="secundaria">
+                  Anterior
+                </Botao>
+              </Col>
+              <Col lg={6} md={6} sm={6}>
+                <div style={{ textAlign: 'right' }}>
+                  <Botao>
+                    Próxima
                   </Botao>
-                </Col>
-                <Col lg={6} md={6} sm={6}>
-                  <div style={{ textAlign: 'right' }}>
-                    <Botao>
-                      Próxima
-                    </Botao>
-                  </div>
-                </Col>
-              </Row>
-            </Card>
-          </Col>
-        </Row>
-      </Container>
+                </div>
+              </Col>
+            </Row>
+          </Card>
+        </Col>
+      </Row>
       <Rodape>
         <Container>
           <Row align="center">
--- src/componentes/ListaSuspensa/ListaSuspensa.jsx
+++ src/componentes/ListaSuspensa/ListaSuspensa.jsx
@@ -1,33 +1,115 @@
-const estadosBrasileiros = [
-    { "text": "Acre", "value": "AC" },
-    { "text": "Alagoas", "value": "AL" },
-    { "text": "Amapá", "value": "AP" },
-    { "text": "Amazonas", "value": "AM" },
-    { "text": "Bahia", "value": "BA" },
-    { "text": "Ceará", "value": "CE" },
-    { "text": "Distrito Federal", "value": "DF" },
-    { "text": "Espírito Santo", "value": "ES" },
-    { "text": "Goiás", "value": "GO" },
-    { "text": "Maranhão", "value": "MA" },
-    { "text": "Mato Grosso", "value": "MT" },
-    { "text": "Mato Grosso do Sul", "value": "MS" },
-    { "text": "Minas Gerais", "value": "MG" },
-    { "text": "Pará", "value": "PA" },
-    { "text": "Paraíba", "value": "PB" },
-    { "text": "Paraná", "value": "PR" },
-    { "text": "Pernambuco", "value": "PE" },
-    { "text": "Piauí", "value": "PI" },
-    { "text": "Rio de Janeiro", "value": "RJ" },
-    { "text": "Rio Grande do Norte", "value": "RN" },
-    { "text": "Rio Grande do Sul", "value": "RS" },
-    { "text": "Rondônia", "value": "RO" },
-    { "text": "Roraima", "value": "RR" },
-    { "text": "Santa Catarina", "value": "SC" },
-    { "text": "São Paulo", "value": "SP" },
-    { "text": "Sergipe", "value": "SE" },
-    { "text": "Tocantins", "value": "TO" }
-  ]
+import styled from "@emotion/styled"
+import { useState } from "react"
+import { ItemListaSuspensaEstilizado } from "./ItemListaSuspensaEstilizado"
+import { ListaSuspensaEstilizada } from "./ListaSuspensaEstilizada"
 
-export const ListaSuspensa = () => {
+const LabelEstilizada = styled.label`
+    display: block;
+    width: 100%;
+    box-sizing: border-box;
+    font-weight: 400;
+    font-size: 20px;
+    line-height: 24px;
+    position: relative;
 
+`
+
+const BotaoEstilizado = styled.button`
+    cursor: pointer;
+    display: block;
+    height: 40px;
+    width: 100%;
+    font-size: 18px;
+    outline: none;
+    box-sizing: border-box;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    font-weight: 400;
+    border-radius: 18px;
+    border-bottom-left-radius: ${props => props.estaAberta ? '0' : '18px'};
+    border-bottom-right-radius: ${props => props.estaAberta ? '0' : '18px'};
+    margin-top: ${props => props.theme.espacamentos.xs};
+    padding: ${props => props.theme.espacamentos.s};
+    background: ${props => props.theme.cores.branco};
+    border: 1px solid ${props => props.theme.cores.neutras.a};
+    &:focus {
+        border-color: ${props => props.theme.cores.focus};
+    }
+`
+
+export const ListaSupensa = ({ titulo, opcoes }) => {
+    const [estaAberta, alternarVisibilidade] = useState(false)
+
+    const [opcaoFocada, setOpcaoFocada] = useState(null);
+    const [opcaoSelecionada, setOpcaoSelecionada] = useState(null);
+
+    const manipularTeclaDoTeclado = (evento) => {
+        alternarVisibilidade(true)
+        switch (evento.key) {
+            case 'ArrowDown':
+                evento.preventDefault();
+                setOpcaoFocada(focoAntigo => {
+                    if (focoAntigo == null) {
+                        return 0;
+                    }
+                    if (focoAntigo === (opcoes.length - 1)) {
+                        return opcoes.length - 1
+                    }
+                    return focoAntigo += 1
+                })
+                break;
+            case 'ArrowUp':
+                evento.preventDefault();
+                setOpcaoFocada(focoAntigo => {
+                    if (!focoAntigo) {
+                        return 0;
+                    }
+                    return focoAntigo -= 1
+                })
+                break;
+                case 'Enter':
+                    evento.preventDefault();
+                    setOpcaoFocada(null)
+                    alternarVisibilidade(false)
+                    setOpcaoSelecionada(opcoes[opcaoFocada])
+                    break;
+                case 'Tab':
+                    setOpcaoFocada(null)
+                    alternarVisibilidade(false)
+                    break;
+                case 'Escape':
+                    evento.preventDefault();
+                    setOpcaoFocada(null)
+                    alternarVisibilidade(false)
+                    break;
+            default:
+                break;
+        }
+    }
+
+    return (<LabelEstilizada>
+        {titulo}
+        <BotaoEstilizado
+             estaAberta={estaAberta}
+             onClick={() => alternarVisibilidade(!estaAberta)}
+             onKeyDown={manipularTeclaDoTeclado}
+        >
+            <div>
+                { opcaoSelecionada ? opcaoSelecionada.text : 'Selecione' } 
+            </div>
+            <div>
+                <span>{estaAberta ? '▲' : '▼'}</span>
+            </div>
+        </BotaoEstilizado>
+        {estaAberta && <ListaSuspensaEstilizada>
+            {opcoes.map((opcao, index) => <ItemListaSuspensaEstilizado
+                key={opcao.value}
+                focoAtivo={index === opcaoFocada}
+                onClick={() => setOpcaoSelecionada(opcao)}
+                >
+                {opcao.text}
+            </ItemListaSuspensaEstilizado>)}
+        </ListaSuspensaEstilizada>}
+    </LabelEstilizada>)
 }
